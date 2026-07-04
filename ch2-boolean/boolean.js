c441b25230d97b8f21439d5c89b3df0af70d8878
lyritzis286 <lyritzis286@gmail.com>
2026-07-04 17:30:11 +0300

    Add explicit conversions and loop condition examples

---
+++ ch2-boolean/boolean.js
@@ -0,0 +1,19 @@
+//Explicit Conversions
+console.log("Boolean(1)" + Boolean(1));
+console.log("Boolean(0) " + Boolean(0));
+console.log("Boolean('') " + Boolean(''));
+console.log("Boolean('Hello') " + Boolean('Hello'));
+
+// Loop Conditions - Iterations
+while(1) {
+    break;
+}
+
+do {
+    console.log("This will run once");
+} while(0);
+
+for(let i = 1; 1; i++) {
+    console.log("In for loop");
+    break;
+}
