92d2dca789ca2d6915cef3775b8513f392d1f53a
lyritzis286 <lyritzis286@gmail.com>
2026-07-04 17:43:16 +0300

    Add grading logic to evaluate student performance

---
+++ ch2-boolean/grades.js
@@ -0,0 +1,11 @@
+const grade = 7.5;
+
+if (grade > 8.5) {
+    console.log("You got an A");
+} else if (grade >= 6.5 ) {
+    console.log("You got a B");
+}else if (grade >=5) {
+    console.log("You got a C");
+}else {
+    console.log("fail");
+}
