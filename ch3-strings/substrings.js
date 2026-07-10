58c795ed44b099a423690a07ee01428f68c640e4
lyritzis286 <lyritzis286@gmail.com>
2026-07-10 14:02:48 +0300

    Add substrings.js to demonstrate string manipulation methods

---
+++ ch3-strings/substrings.js
@@ -0,0 +1,11 @@
+let str = 'Coding Factory'
+
+console.log(str.substring(1, 5));  //odin C = 0 O =1 ... 5 den to pairnei
+console.log(str.slice(1, 5));       //start index - endindex - 1 odin
+console.log(str.slice(2));      //ding Factory
+console.log(str.slice(-7));      //Factory
+
+//split
+let tokens = str.split(' ');
+console.log(tokens)
+
