bb38c58dcb6ca3a88f4bcbaec87947a316772409
lyritzis286 <lyritzis286@gmail.com>
2026-07-10 15:17:15 +0300

    Add slice.js to demonstrate array slicing and copying methods

---
+++ ch4-arrays/slice.js
@@ -0,0 +1,13 @@
+const cities = ['Athens', 'Patras', 'Larissa', 'Volos']
+
+console.log(cities.slice(1, 3));
+console.log(cities.slice(2));
+console.log(cities.slice(-2))
+
+// ============ shallow copy
+const copyCities = cities.slice();
+const copyCities2 = [...cities];    //Modern way to copy
+console.log(copyCities2);
+
+const orders = [ {total: 10}, {total: 20} ]
+const deepOrdersCopy = structuredClone(orders)
