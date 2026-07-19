d813fca90cf7a82177724e11674265137f21e36c
lyritzis286 <lyritzis286@gmail.com>
2026-07-19 18:53:57 +0300

    Add conditional checks for book author properties and utilize nullish coalescing

---
+++ ch5-objects/conditional-checks.js
@@ -0,0 +1,22 @@
+const book = {
+    author: {firstname: "", lastname: ""}
+}
+
+const book2 = {}
+
+let book2Author = book2.author.lastname
+
+if(book2) {
+    if(book2.author) {
+        authorLast = book2.author.lastname
+        }
+}
+
+authorLast = book2?.author?.lastname //return undefined
+
+//Nullish coalescing operator
+authorLast = book2?.author?.lastname ?? "Unknown"
+
+if(book.hasOwnProperty('author')) {
+    console.log(book.author)
+}
