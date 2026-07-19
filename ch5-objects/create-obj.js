496d9fa9d20edecf9607e0a42a2e586607e1f2eb
lyritzis286 <lyritzis286@gmail.com>
2026-07-19 18:26:59 +0300

    Add initial object creation and logging for point coordinates

---
+++ ch5-objects/create-obj.js
@@ -0,0 +1,14 @@
+const obj = {} //empty object
+const point = {x : 1, y : 2}
+point.z = 4;
+
+console.log(point)
+
+const person  = {
+    firstname: "Alice",
+    lastname: "W.",
+    hobbies: [tennis, football, piano],
+    home: { x: 234567.65, y: 334456.99 },
+    geometry: [{x: 1234, y: 2345}, {x: 345632, y: 324521}, 
+        {x: 1243, y: 65839}]
+}
