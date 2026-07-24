f8da86860f279cc2733f03c09a64b91ab3152e54
lyritzis286 <lyritzis286@gmail.com>
2026-07-24 17:31:27 +0300

    Add user object with methods for retrieving first name and full name

---
+++ ch5-objects/object-methods.js
@@ -0,0 +1,14 @@
+const user = {
+    firstname: "George",
+    lastname: "Papadopoulos",
+    getFirstname: function() {
+        return this.firstname
+    },
+    getFullname() {
+        return `${this.firstname}, ${this.lastname}`
+    }
+}
+
+console.log(user.getFirstname())
+
+console.log(user.getFullname())
