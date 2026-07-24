64542b575ec7a56ca68215080dae41f9f0d709c2
lyritzis286 <lyritzis286@gmail.com>
2026-07-24 17:22:21 +0300

    Add destructuring examples for arrays and objects in JavaScript

---
+++ ch5-objects/destructuring.js
@@ -0,0 +1,14 @@
+// const a = 7
+// const b = 5;
+const [a, b] = [7, 5]
+
+const person = {
+    firstname: "Alice",
+    age: 25,
+    city: "Athens"
+}
+
+//Object Destructuring
+const {firstname, age, city} = person
+
+console.log(firstname)
