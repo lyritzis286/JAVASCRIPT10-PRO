147035ddf1d3081eb11aba6db09c9386d4d55aa9
lyritzis286 <lyritzis286@gmail.com>
2026-07-24 17:17:51 +0300

    Refactor object copying examples to demonstrate shallow and deep copy effects

--- ch5-objects/copy-objects.js
+++ ch5-objects/copy-objects.js
@@ -4,12 +4,19 @@ const initial = {
     address: {street: "Patission", streetNum: 80}
 }
 
+const copyObj = initial; 
+
+copyObj.address= {street: "Vikela", streetNum: 90}
+console.log(initial.address)
+
 const copyInitial = { ...initial }        //SHALLOW COPY
-console.log(copyInitial)
+copyInitial.firstname = 'Bob'
+copyInitial.address.street = 'Aristotelous'
+console.log(initial.address.street)
 
 const copyInitial2 = JSON.parse(JSON.stringify(initial)) //deep copy
 console.log(copyInitial2)
 
-const copyInitial3 = structuredClone(initial)
+const copyInitial3 = structuredClone(initial) //deep copy
 
 console.log(copyInitial3)
