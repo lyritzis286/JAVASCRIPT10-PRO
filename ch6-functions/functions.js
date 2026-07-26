0f4ce130c28c5b4ad1122dacf003430dcbcd6ea8
lyritzis286 <lyritzis286@gmail.com>
2026-07-26 15:13:20 +0300

    Add examples for optional and default parameters in function definitions

--- ch6-functions/functions.js
+++ ch6-functions/functions.js
@@ -18,3 +18,19 @@ const multiply = (a, b) => a * b;
 
 console.log(multiply(3, 7))
 
+//Optional Params
+function createButton(text, color) {
+    color = color || 'blue'
+    console.log(text, color)
+}
+
+createButton('Hello', '') 
+
+//Default Params
+function createButton2(text, color = 'blue') {
+    console.log(text, color)
+}
+
+createButton2('Hello')      //Hello blue
+createButton2('Hello', 'red ')           //Hello red
+createButton2('Hello', '')      //Hello
