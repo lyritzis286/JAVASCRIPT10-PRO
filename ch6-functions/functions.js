81e82ee7111044c7e11151c9256be841f64ac706
lyritzis286 <lyritzis286@gmail.com>
2026-07-26 15:37:57 +0300

    Add examples for rest parameters and total price calculation using spread syntax

--- ch6-functions/functions.js
+++ ch6-functions/functions.js
@@ -34,3 +34,50 @@ function createButton2(text, color = 'blue') {
 createButton2('Hello')      //Hello blue
 createButton2('Hello', 'red ')           //Hello red
 createButton2('Hello', '')      //Hello
+
+//Rest parameters
+//let arr = [1, 2, 3, 4]
+//max(...arr)
+//max(1, 2, 3, 4, 6)
+ function max(...numbers) {          //rest operator
+    // for(const num of numbers) {
+    //     // find max 
+    // }
+    return Math.max(...numbers)     //spread operator
+
+} 
+
+ console.log(max(1, 2, 8, 3, 23, 17))  //23
+
+
+ function calculateTotalPrice(discount, ...prices) {
+    const totalPrice = prices.reduce((sum, price) => sum + price, 0)
+
+    return totalPrice - discount
+ }
+
+ console.log(calculateTotalPrice(5, 10, 8, 30, 7))
+
+ //Sanity Checks στα Input params των συναρτησεων
+//εχουν νοημα μονο αν  public APIs, external data
+const user = {
+    id: 1,
+    fistname: 'Alice',
+    lastname: 'W.',
+    hobbies: ['reading']
+}
+// Legacy
+function getFirstHobby(user) {
+    if (!(user && user.hobbies &&Array.isArray(user.hobbies) 
+        && user.hobbies.length > 0)) {
+        return
+    }
+
+    return user.hobbies[0]
+}
+
+// Modern way
+
+function getFirstHobby2(user) {
+    return user?.hobbies?.[0] ?? 'No Hobbies'
+}
