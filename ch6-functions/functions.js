b36e1523aa956cb4d569b8bec939be8de21078a6
lyritzis286 <lyritzis286@gmail.com>
2026-07-26 15:01:15 +0300

    Add basic function examples including function syntax, expressions, and arrow functions

---
+++ ch6-functions/functions.js
@@ -0,0 +1,20 @@
+//Basic function syntax
+function sayHello(name) {
+    return `Hello ${name}`
+}
+
+console.log(sayHello('Alice'))
+
+//2 function expression 
+
+const add = function(a, b) {
+    return a + b
+}
+
+console.log(add(3, 8))
+
+//Arrow functions 
+const multiply = (a, b) => a * b;
+
+console.log(multiply(3, 7))
+
